85d77070b3ed0ec528b8ed92778e4b8916e715fa
angelivan_spartan <angelivan_spartan@hotmail.com>
2022-12-17 22:57:27 -0600

    added .net7 to dwsl

--- channels/deps_dwsl.sh
+++ channels/deps_dwsl.sh
@@ -10,3 +10,22 @@ chmod 4755 /sbin/reboot
 apt -y update
 apt -y upgrade
 apt -y install exfat-fuse python3 libncurses5 fte-terminal
+
+if [[ -d /usr/bin/dotnet7 ]]; then
+echo -e "" 
+else
+echo "Installing .Net7..."
+dotnetver=7.0
+cd /tmp/
+wget https://dot.net/v1/dotnet-install.sh
+bash dotnet-install.sh --channel 7.0
+rm dotnet-install.sh
+mv ~/.dotnet /usr/bin/dotnet7
+ln -sf /usr/bin/dotnet7/dotnet /usr/bin/dotnet
+if grep -q 'export DOTNET_ROOT=' /etc/environment;  then
+  echo 'Already added link to environment'
+else
+  echo 'export DOTNET_ROOT=/usr/bin/dotnet7' >> /etc/environment
+fi
+export DOTNET_ROOT=/usr/bin/dotnet7
+fi
