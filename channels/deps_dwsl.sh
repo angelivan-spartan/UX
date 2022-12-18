c5fee31221389e5d2372bbef420ab3a2385712d1
angelivan_spartan <angelivan_spartan@hotmail.com>
2022-12-17 23:21:30 -0600

    added fbc to dwsl

--- channels/deps_dwsl.sh
+++ channels/deps_dwsl.sh
@@ -9,7 +9,22 @@ chmod 4755 /sbin/reboot
 
 apt -y update
 apt -y upgrade
-apt -y install exfat-fuse python3 libncurses5 fte-terminal
+apt -y install exfat-fuse python3 libncurses5 fte-terminal libfreebasic libxext-dev libncurses5-dev libx11-dev libxpm-dev libxrandr-dev libstdc++6-4.4-dev
+
+if [[ -d /usr/local/include/freebasic ]]; then
+echo -e "" 
+else
+echo "Installing FBC..."
+cd /tmp
+wget http://nchc.dl.sourceforge.net/project/fbc/Binaries%20-%20Linux/FreeBASIC%200.21.1/FreeBASIC-0.21.1-linux.tar.lzma
+tar -xvf FreeBASIC-0.21.1-linux.tar.lzma
+cd FreeBASIC-0.21.1-linux
+chmod +x install.sh
+ ./install.sh -i
+cd ..
+rm -rf FreeBASIC-0.21.1-linux
+rm -f FreeBASIC-0.21.1-linux.tar.lzma
+fi
 
 if [[ -d /usr/bin/dotnet7 ]]; then
 echo -e "" 
@@ -22,10 +37,12 @@ bash dotnet-install.sh --channel 7.0
 rm dotnet-install.sh
 mv ~/.dotnet /usr/bin/dotnet7
 ln -sf /usr/bin/dotnet7/dotnet /usr/bin/dotnet
-if grep -q 'export DOTNET_ROOT=' /etc/environment;  then
-  echo 'Already added link to environment'
+if grep -q 'export DOTNET_ROOT=' /etc/profile;  then
+  echo 'Already added link to profile'
 else
-  echo 'export DOTNET_ROOT=/usr/bin/dotnet7' >> /etc/environment
+  echo 'export DOTNET_ROOT=/usr/bin/dotnet7' >> /etc/profile
 fi
 export DOTNET_ROOT=/usr/bin/dotnet7
 fi
+
+
