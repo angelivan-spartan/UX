ac77a29a0a3b124d72a03d21143e9e1f10e33625
angelivan_spartan <angelivan_spartan@hotmail.com>
2022-12-18 21:57:02 -0600

    fixed pkg name

--- channels/deps_rp32.sh
+++ channels/deps_rp32.sh
@@ -9,7 +9,7 @@ chmod 4755 /sbin/reboot
 
 apt -y update
 apt -y upgrade
-apt -y install exfat-fuse python3 libncurses5 fte-terminal libgbm1 libgl1-mesa-dri libegl1-mesa libinput10 libICE.so.6 kmscube
+apt -y install exfat-fuse python3 libncurses5 fte-terminal libgbm1 libgl1-mesa-dri libegl1-mesa libinput10 libice6 kmscube
 
 
 if [[ -d /usr/local/include/freebasic ]]; then
