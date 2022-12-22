e91e7bd2ed83c931b949618821d78a00727f82bb
angelivan_spartan <angelivan_spartan@hotmail.com>
2022-12-22 00:15:46 -0600

    removed sh files

--- channels/platform_agnostic.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [[ $EUID -ne 0 ]]; then
-   echo -e "\e[1;31mThis program must be run as root" 
-   exit 1
-fi
-echo "Updating System..."
-cp -r /usr/src/UX/SRC/base/* /
-chmod +x /usr/bin/uxdate
-ln -sf /etc/update-motd.d/10-uname /usr/bin/version
